7c13e0135a34be3ffcc5efc623a1adee153e3fa9
45Yos <tyossi91@gmail.com>
2025-01-05 03:27:20 +0200

    update

---
+++ games/BackButtonGames.js
@@ -0,0 +1,24 @@
+const backToHomeDiv = document.createElement("div");
+backToHomeDiv.id = 'backToHome';
+backToHomeDiv.className = "back-to-home";
+
+const backToHomeLink = document.createElement("a");
+backToHomeLink.href = "../../index.html";
+backToHomeLink.innerText = "Back to Home";
+
+const backToProjectsDiv = document.createElement("div");
+backToProjectsDiv.className = "back-to-home";
+backToProjectsDiv.id = 'backToProjects';
+
+
+const backToProjectsLink = document.createElement("a");
+backToProjectsLink.href = "../MyGames.html";
+backToProjectsLink.innerText = "Back to Games";
+
+
+backToHomeDiv.appendChild(backToHomeLink);
+
+backToProjectsDiv.appendChild(backToProjectsLink);
+
+document.body.appendChild(backToHomeDiv);
+document.body.appendChild(backToProjectsDiv);
